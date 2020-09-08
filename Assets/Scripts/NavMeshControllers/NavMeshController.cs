e5f7d369cb7baab997e5ca6e03bdfe0579ea1695
Jantix6 <apujantedp@gmail.com>
2020-09-08 18:11:38 +0200

    Movimiento básico

---
+++ Assets/Scripts/NavMeshControllers/NavMeshController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class NavMeshController : MonoBehaviour
+{
+    public Transform[] routePoints;
+    NavMeshAgent agent;
+    Transform nextPos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        nextPos = routePoints[Random.Range(0, routePoints.Length - 1)];
+        agent.destination = nextPos.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(!agent.hasPath)
+        {
+            Transform currentPos = nextPos;
+            nextPos = routePoints[Random.Range(0, routePoints.Length - 1)];
+
+            if (nextPos != currentPos)
+            {
+                agent.destination = nextPos.position;
+            }
+        }
+    }
+}
