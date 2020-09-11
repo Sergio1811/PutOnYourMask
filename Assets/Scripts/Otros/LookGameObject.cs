51f48853612ba7de4d870dcabf21613c320b6359
Sergio Rodriguez <pekeno1811@gmail.com>
2020-09-11 10:32:41 +0200

    Assets para cooking lab

---
+++ Assets/Scripts/Otros/LookGameObject.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookGameObject : MonoBehaviour
+{
+
+    public GameObject objectToLookAt;
+    private Vector3 targetPoint;
+    private Quaternion targetRotation;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        targetPoint = new Vector3(objectToLookAt.transform.position.x, transform.position.y, objectToLookAt.transform.position.z) - transform.position;
+        targetRotation = Quaternion.LookRotation(targetPoint, Vector3.up);
+        transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, Time.deltaTime * 2.0f);
+
+    }
+}
