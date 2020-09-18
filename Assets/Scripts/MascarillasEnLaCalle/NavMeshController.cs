553bc5cdacea2fda7018316350fc8bbca9fd45b3
Jantix6 <apujantedp@gmail.com>
2020-09-18 17:23:41 +0200

    IA movement

---
+++ Assets/Scripts/MascarillasEnLaCalle/NavMeshController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class NavMeshController : MonoBehaviour
+{
+    public Transform[] routePoints;
+    NavMeshAgent agent;
+    Transform nextPos;
+    NavMeshPath navMeshPath;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        nextPos = routePoints[Random.Range(0, routePoints.Length - 1)];
+        agent.destination = nextPos.position;
+        navMeshPath = agent.path;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(!agent.hasPath || navMeshPath.status != NavMeshPathStatus.PathComplete)
+        {
+            CreateNewPath();
+        }
+    }
+
+    void CreateNewPath()
+    {
+        Transform currentPos = nextPos;
+        nextPos = routePoints[Random.Range(0, routePoints.Length - 1)];
+
+        if (nextPos != currentPos)
+        {
+            agent.destination = nextPos.position;
+            navMeshPath = agent.path;
+        }
+    }
+}
