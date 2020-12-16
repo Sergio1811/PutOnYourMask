0e2da03c0385ebc57fc492ae6d7543b0c883b65b
Sergio Rodriguez <pekeno1811@gmail.com>
2020-12-16 20:47:48 +0100

    Mas ui menu

---
+++ Assets/Scripts/Otros/ButtonPrueba.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonPrueba : MonoBehaviour
+{
+    public void PlayAnim(Animation anim)
+    {
+        anim.Play();
+    }
+
+    public void PlayAnimationDerecha(Animation anim)
+    {
+        anim.clip = anim.GetClip("SlideEnBarra");
+        anim.Play();
+    }
+    public void PlayAnimationIzquierda(Animation anim)
+    {
+        anim.clip = anim.GetClip("SlideEnBarraInverso");
+        anim.Play();
+    }
+}
