8578265a6a926f4b78c4cc1750105f6e96267f77
Jantix6 <apujantedp@gmail.com>
2021-04-19 15:20:28 +0200

    RandomCharGen
    Not tested properly

---
+++ Assets/Scripts/RandomCharacters/CharacterGenerator.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterGenerator : MonoBehaviour
+{
+    public static CharacterGenerator instance;
+
+    [SerializeField] private Material[] cloths;
+    [SerializeField] private Material[] skinColors;
+
+    [SerializeField] private GameObject characterPrefab;
+    [SerializeField] private Transform parentTransform;
+
+    private void Awake()
+    { 
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void GenerateRandomCharacter()
+    {
+        RandomCharacter randomCharacter = Instantiate(characterPrefab, parentTransform).GetComponent<RandomCharacter>();
+        foreach (BodyPart item in System.Enum.GetValues(typeof(BodyPart)))
+        {
+            randomCharacter.ChangeClothes(cloths[Random.Range(0, cloths.Length)], item);
+        }
+        randomCharacter.ChangeSkinsColor(skinColors[Random.Range(0, skinColors.Length)]);
+    }
+}
---
+++ Assets/Scripts/RandomCharacters/RandomCharacter.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomCharacter : MonoBehaviour
+{
+    [SerializeField] private SkinnedMeshRenderer head;
+    [SerializeField] private SkinnedMeshRenderer body;
+    [SerializeField] private SkinnedMeshRenderer cap;
+    [SerializeField] private SkinnedMeshRenderer legs;
+    [SerializeField] private SkinnedMeshRenderer toes;
+
+    public void ChangeClothes(Material newCloth, BodyPart bodyPart)
+    {
+        SkinnedMeshRenderer meshToChange;
+
+        switch (bodyPart)
+        {
+            case BodyPart.BODY:
+                meshToChange = body;
+                break;
+            case BodyPart.CAP:
+                meshToChange = cap;
+                break;
+            case BodyPart.LEGS:
+                meshToChange = legs;
+                break;
+            case BodyPart.TOES:
+                meshToChange = toes;
+                break;
+            default:
+                meshToChange = null;
+                break;
+        }
+
+        if (meshToChange != null)
+            meshToChange.materials[0] = newCloth;
+    }
+
+    public void ChangeSkinsColor(Material newSkinColor)
+    {
+        head.material = newSkinColor;
+        body.materials[1] = newSkinColor;
+        legs.materials[1] = newSkinColor;
+    }
+}
