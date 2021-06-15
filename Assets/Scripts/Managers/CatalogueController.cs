c5ff530cea5399770d9abf435d19cc5164c1ca44
Sergio1811 <pekeno1811@gmail.com>
2021-06-15 18:59:17 +0200

    avance catalgo

---
+++ Assets/Scripts/Managers/CatalogueController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class CatalogueController : MonoBehaviour
+{
+    public GameObject[] postIts;
+    public GameObject[] collections;
+    public Transform parent;
+
+    public void PostItClicked(GameObject postItClicked)
+    {
+        foreach (var item in postIts)
+        {
+            item.transform.SetAsFirstSibling();
+        }
+        foreach (var item in collections)
+        {
+            item.SetActive(false);
+        }
+
+        postItClicked.transform.SetSiblingIndex(postIts.Length);
+        postItClicked.GetComponent<SectionActivator>().section.SetActive(true);
+    }
+}
