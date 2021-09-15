5f0fc1e07c8cbd50b7c2c02d461c0bb17b796d93
Sergio1811 <pekeno1811@gmail.com>
2021-09-15 18:35:09 +0200

    juego con icon

---
+++ Assets/Scripts/Otros/FillIcon.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FillIcon : MonoBehaviour
+{
+    public Slider slider;
+    public Image virusSatur;
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        virusSatur.fillAmount = slider.value;
+    }
+}
